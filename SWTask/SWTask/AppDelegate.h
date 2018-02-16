f83ca84972b591e7a96593478507aeb12e6036f4
Artsemi Ryzhankou <artsemi_ryzhankou@epam.com>
2018-02-16 13:01:58 +0300

    clean project

---
+++ SWTask/SWTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWTask
+//
+//  Created by Artsemi Ryzhankou on 2/16/18.
+//  Copyright © 2018 ArtsemiRyzhankou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWTask/SWTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWTask
+//
+//  Created by Artsemi Ryzhankou on 2/16/18.
+//  Copyright © 2018 ArtsemiRyzhankou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
